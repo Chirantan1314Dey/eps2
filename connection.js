2b954d841a34bec3501c18d9f8f3e73515f4a558
Chirantan1314Dey <126395439+Chirantan1314Dey@users.noreply.github.com>
2023-04-20 13:08:23 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"collage"
+    host:"sql309.epizy.com",
+    user:"epiz_34047971",
+    password:"xbyXYE4bhbjnS",
+    database:"epiz_34047971_collage"
   });
 
   module.exports = con;
